6c29e833b640077a6db5b48c0e3457514921bac0
emmandeville <43049034+emmandeville@users.noreply.github.com>
2018-12-05 23:24:54 -0500

    Add files via upload

--- nycicecream/script.js
+++ nycicecream/script.js
@@ -3,12 +3,84 @@ $(document).ready(function() {
     $("body").toggleClass("blue");
   });
   $(".buttonprice").hover(function() {
-    $("body").toggleClass("purple");
-  });
-  $(".buttonphotos").hover(function() {
     $("body").toggleClass("peach");
   });
   $(".buttonlocation").hover(function() {
     $("body").toggleClass("green");
   });
+    $(".buttonlocation").hover(function() {
+    $(".labellocation").toggleClass("bright");
+  });
+    $(".buttoninfo").hover(function() {
+    $(".labelinfo").toggleClass("bright");
+  });
+    $(".buttonprice").hover(function() {
+    $(".labelprice").toggleClass("bright");
+  });
+    $(".pinone").hover(function() {
+    $(".eddiespic").toggleClass("brightpic");
+  });
+    $(".pinone").hover(function() {
+    $(".eddieslabel").toggleClass("bright");
+  });
+    $(".pintwo").hover(function() {
+    $(".minapic").toggleClass("brightpic");
+  });
+    $(".pintwo").hover(function() {
+    $(".minalabel").toggleClass("bright");
+  });
+    $(".pinthree").hover(function() {
+    $(".biggpic").toggleClass("brightpic");
+  });
+    $(".pinthree").hover(function() {
+    $(".bigglabel").toggleClass("bright");
+  });
+    $(".pinfour").hover(function() {
+    $(".eggpic").toggleClass("brightpic");
+  });
+    $(".pinfour").hover(function() {
+    $(".egglabel").toggleClass("bright");
+  });
+    $(".pinfive").hover(function() {
+    $(".tenpic").toggleClass("brightpic");
+  });
+    $(".pinfive").hover(function() {
+    $(".tenlabel").toggleClass("bright");
+  });
+    $(".pinsix").hover(function() {
+    $(".icepic").toggleClass("brightpic");
+  });
+    $(".pinsix").hover(function() {
+    $(".icelabel").toggleClass("bright");
+  });
+    $(".pinseven").hover(function() {
+    $(".amorpic").toggleClass("brightpic");
+  });
+    $(".pinseven").hover(function() {
+    $(".amorlabel").toggleClass("bright");
+  });
+    $(".pineight").hover(function() {
+    $(".hillpic").toggleClass("brightpic");
+  });
+    $(".pineight").hover(function() {
+    $(".hilllabel").toggleClass("bright");
+  });
+    $(".pinnine").hover(function() {
+    $(".taipic").toggleClass("brightpic");
+  });
+    $(".pinnine").hover(function() {
+    $(".tailabel").toggleClass("bright");
+  });
+    $(".pinten").hover(function() {
+    $(".oddpic").toggleClass("brightpic");
+  });
+    $(".pinten").hover(function() {
+    $(".oddlabel").toggleClass("bright");
+  });
+    $(".pineleven").hover(function() {
+    $(".eggerpic").toggleClass("brightpic");
+  });
+    $(".pineleven").hover(function() {
+    $(".eggerlabel").toggleClass("bright");
+});
 });
