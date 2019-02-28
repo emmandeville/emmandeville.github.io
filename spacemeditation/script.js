9a1b84a4a7bf7e9c335bfd919c47cb8c5e1fa448
emmandeville <43049034+emmandeville@users.noreply.github.com>
2019-02-28 14:57:59 -0500

    Add files via upload

--- spacemeditation/script.js
+++ spacemeditation/script.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
   setTimeout(function(){
+  	$(".button").removeClass("off");
+    $(".launch").removeClass("off");
     $(".launch").addClass("alarming"); 
     $(".button").addClass("alarming");
-  }, 5000);
+    $(".rocket").addClass("off");
+    $(".back").addClass("alarming");
+  }, 30000);
 
   $(".button").click(function() { 
     $(".launch").removeClass("alarming");
     $(".button").removeClass("alarming");
+    $(".back").removeClass("alarming");
+    $(".launch").addClass("off");
+    $(".button").addClass("off");
+    $(".rocket").removeClass("off");
   });
 
 });
