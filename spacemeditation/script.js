f1d5b12d0a6defcd8ded996d154bff14f6c4769d
emmandeville <43049034+emmandeville@users.noreply.github.com>
2019-03-10 16:02:53 -0400

    Add files via upload

--- spacemeditation/script.js
+++ spacemeditation/script.js
@@ -2,19 +2,36 @@ $(document).ready(function() {
   setTimeout(function(){
   	$(".button").removeClass("off");
     $(".launch").removeClass("off");
+    $(".alert").removeClass("off");
     $(".launch").addClass("alarming"); 
     $(".button").addClass("alarming");
     $(".rocket").addClass("off");
     $(".back").addClass("alarming");
+    $(".alert").addClass("alarming");
+    $("audio").get(0).play();
   }, 30000);
 
   $(".button").click(function() { 
     $(".launch").removeClass("alarming");
     $(".button").removeClass("alarming");
+    $(".alert").removeClass("alarming");
     $(".back").removeClass("alarming");
     $(".launch").addClass("off");
     $(".button").addClass("off");
+    $(".alert").addClass("off");
     $(".rocket").removeClass("off");
+    $("audio").get(0).pause();
+    $("audio").get(0).currentTime = 0;
+    setTimeout(function(){
+    $(".button").removeClass("off");
+    $(".launch").removeClass("off");
+    $(".alert").removeClass("off");
+    $(".launch").addClass("alarming"); 
+    $(".button").addClass("alarming");
+    $(".rocket").addClass("off");
+    $(".back").addClass("alarming");
+    $(".alert").addClass("alarming");
+  }, 30000);
   });
 
 });
