654c38d197568b1ddcba495160802c06eb942337
emmandeville <43049034+emmandeville@users.noreply.github.com>
2019-03-27 17:36:40 -0400

    Add files via upload

--- oceanoctogame/script.js
+++ oceanoctogame/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({makeResponsive: true, heroSpeed: 300, monsterSpeed: 400, attackRadius: 100, monsterHitRadius: 100, attackDuration:1000, jumpHeight: 300});
+  mugen.initialize({makeResponsive: true, heroSpeed: 300, monsterSpeed: 300, timeBetweenMonsters: 4000,
+  	attackRadius: 100, monsterHitRadius: 100, attackDuration: 650, jumpHeight: 240});
 });
