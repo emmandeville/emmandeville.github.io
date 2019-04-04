699fb1e1ac622791ed9ea0e2bf3028d086c670aa
emmandeville <43049034+emmandeville@users.noreply.github.com>
2019-04-04 12:44:28 -0400

    Add files via upload

--- oceanoctogame/script.js
+++ oceanoctogame/script.js
@@ -1,5 +1,23 @@
+var changeBackground = function(){
+	$(".background").addClass("special");
+
+};
+var restoreBackground = function(){
+	$(".background").removeClass("special");
+
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL:"./img/title-screen.png", makeResponsive: true, heroSpeed: 300, monsterSpeed: 300, timeBetweenMonsters: 4500,
-  	attackRadius: 100, monsterHitRadius: 100, attackDuration: 500, jumpHeight: 270});
+  mugen.initialize({titleScreenImageURL:"./img/title-screen.png", 
+  	makeResponsive: true, 
+  	heroSpeed: 300, 
+  	monsterSpeed: 300, 
+  	timeBetweenMonsters: 4500,
+  	attackRadius: 100, 
+  	monsterHitRadius: 100, 
+  	attackDuration: 500, 
+  	jumpHeight: 270, 
+  	onMonsterSpawn: changeBackground, 
+  	onMonsterWasHit: restoreBackground });
 });
